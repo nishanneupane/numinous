f0f20c7c8c321837d0a2ccbc1cd143305515244f
unknown <nishanneupane368@gmail.com>
2024-01-12 19:52:37 +0545

    update : prompt & user

--- app/(companions)/(routes)/companions/[companionId]/page.tsx
+++ app/(companions)/(routes)/companions/[companionId]/page.tsx
@@ -3,6 +3,8 @@ import { db } from '@/lib/db';
 import React from 'react'
 import ChatForm from './_components/chat-form';
 import { redirect } from 'next/navigation';
+import { Badge } from '@/components/ui/badge';
+import { X } from 'lucide-react';
 
 const CompanionIdPage = async ({ params }: { params: { companionId: string } }) => {
     
@@ -29,6 +31,13 @@ const CompanionIdPage = async ({ params }: { params: { companionId: string } }) 
             </div>
             <div className='w-full px-2'>
                 <ChatForm firstQuestion={companion.firstQuestion} prompt={companion.prompt} user={user} />
+            </div>
+
+            <div className="fixed top-0 right-2 text-xs">
+                <Badge variant={"secondary"} className='text-xs font-light cursor-auto'>
+                    <X className='text-rose-600 w-5 h-5 mr-2'/>
+                    Don&apos;t refresh or else your conversation will be deleted
+                </Badge>
             </div>
         </div>
     )
