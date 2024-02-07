716d83fa03caf95fa79493310a6471ef29e29a0f
unknown <nishanneupane368@gmail.com>
2024-02-08 00:07:05 +0545

    update : title edit logic

--- app/(dashboard)/(routes)/teacher/_components/title-edit-form.tsx
+++ app/(dashboard)/(routes)/teacher/_components/title-edit-form.tsx
@@ -38,7 +38,7 @@ const TitleEditForm = ({ item }: TitleEditFormProps) => {
             await axios.patch(`/api/teacher/${item.id}`, values).then((res) => {
                 toast.success(`Profile ${res.data.title} created`)
                 setIsEditing(false)
-                setTitle(title)
+                setTitle(res.data.title)
                 form.reset()
                 router.refresh()
             })
