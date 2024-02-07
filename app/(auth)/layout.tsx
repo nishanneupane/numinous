c976a14cfe50ae6be7380ba9ddba168c52d7b4d5
unknown <nishanneupane368@gmail.com>
2024-02-07 10:22:44 +0545

    update : auth style

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Image from 'next/image'
 import React from 'react'
 
 export const metadata: Metadata = {
@@ -6,7 +7,14 @@ export const metadata: Metadata = {
 }
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='w-full h-full flex items-center justify-center mt-12'>
+        <div className='w-full h-full flex items-center justify-center mt-12 px-6 space-x-3'>
+            <Image
+                src={"/smile.png"}
+                height={300}
+                width={400}
+                alt='Demo'
+                className='object-fit'
+            />
             {children}
         </div>
     )
