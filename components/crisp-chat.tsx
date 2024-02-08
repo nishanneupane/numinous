44a6d6f4977da2f7c0568273c755ab500233c6e2
unknown <nishanneupane368@gmail.com>
2024-02-08 10:39:06 +0545

    update : added crisp chat

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("15a4f3b1-c1ab-434b-b5f9-76876ccf37e6");
+    Crisp.configure("2ff36f4f-0057-4f5f-a761-2b16dc93fbfd");
   }, []);
 
   return null;
