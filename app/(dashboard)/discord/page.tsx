ddd706f15130e33f636481098ff07beca8732bc5
unknown <nishanneupane368@gmail.com>
2024-02-08 15:51:11 +0545

    update : discord  update

--- app/(dashboard)/discord/page.tsx
+++ app/(dashboard)/discord/page.tsx
@@ -8,7 +8,7 @@ const DiscordPage = () => {
             className='w-full md:w-[80%] h-screen flex items-center justify-center flex-col space-y-3 m-auto px-3 md:px-0'
         >
             <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold bg-gradient-to-t from-teal-400 to-blue-600 bg-clip-text text-transparent'>Discord community</h1>
-            <p className='text-muted-foreground md:text-lg text-sm'>Join the Discord community to chat with other students, ask questions, and more.</p>
+            <p className='text-muted-foreground md:text-base text-sm'>Join Discord if you face any queries . Our team is here to help you.</p>
 
             <Link href={"https://discord.gg/xmQx49kw"} className='w-full flex items-center md:justify-center justify-start'>
                 <Button className='w-[50%] flex items-center md:justify-center justify-start bg-gradient-to-t from-teal-400 to-blue-600 text-white font-bold truncate'>
